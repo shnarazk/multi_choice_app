aa4a508bf3f40d8a38d028214b4c9812c27f9374
shujinarazaki <shujinarazaki@protonmail.com>
2020-11-08 19:56:32 +0900

    (ChoiceQuiz) update icons; small tweaks on ContentView
    	modified:   ChoiceQuiz/ChoiceQuiz.xcodeproj/project.xcworkspace/xcuserdata/nash.xcuserdatad/UserInterfaceState.xcuserstate
    	modified:   ChoiceQuiz/ChoiceQuiz/Assets.xcassets/AppIcon-1.appiconset/ChoiceQuize-1.png
    	modified:   ChoiceQuiz/ChoiceQuiz/Assets.xcassets/AppIcon-1.appiconset/ChoiceQuize.png
    	modified:   ChoiceQuiz/ChoiceQuiz/Assets.xcassets/AppIcon-1.appiconset/Contents.json
    	modified:   ChoiceQuiz/ChoiceQuiz/Assets.xcassets/Icon.iconset/icon_512x512.png
    	modified:   ChoiceQuiz/ChoiceQuiz/ContentView.swift

--- ChoiceQuiz/ChoiceQuiz/ContentView.swift
+++ ChoiceQuiz/ChoiceQuiz/ContentView.swift
@@ -12,58 +12,51 @@ struct ContentView: View {
     @State var quiz: Quiz
     @State var problems = 1
     @State var corrects = 0
-    // @State var title: String = "swift"
     var body: some View {
         ZStack {
-            VStack {
-                // Text(title)
-                NavigationView {
-                    List {
-                        Text(quiz.text)
-                            .font(.headline)
-                            .foregroundColor(.red)
-                            .padding(.bottom, 8)
-                        ForEach(quiz.choice.shuffled(), id: \.self) { c in
-                            NavigationLink(
-                                destination:
-                                    ZStack {
-                                        Text(c.correct() ? "⭕️" : "❌")
-                                            .font(Font.system(size: 300))
-                                            .opacity(0.75)
-                                            .padding()
-                                        Text("\(c.correct() ? corrects + 1 : 0)問連続正解")
-                                            .font(.title)
-                                    }
-                                    .onAppear {
-                                        self.quiz = quizes.randomElement()!
-                                        self.problems += 1
-                                        self.corrects = c.correct() ? self.corrects + 1 : 0
-                                    },
-                                label: {
-                                    HStack {
-                                        Image(systemName: "arrow.forward.circle.fill")
-                                        Text(c.dropFirst())
-                                    }
+            NavigationView {
+                List {
+                    Text(quiz.text)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                        .padding(.vertical, 20)
+                    ForEach(quiz.choice.shuffled(), id: \.self) { c in
+                        NavigationLink(
+                            destination:
+                                ZStack {
+                                    Text(c.correct() ? "⭕️" : "❌")
+                                        .font(Font.system(size: 300))
+                                        .opacity(0.75)
+                                        .padding()
+                                    Text("\(c.correct() ? corrects + 1 : 0)問連続正解")
+                                        .font(.title)
                                 }
-                            )
-                        }
+                                .onAppear {
+                                    self.quiz = quizes.randomElement()!
+                                    self.problems += 1
+                                    self.corrects = c.correct() ? self.corrects + 1 : 0
+                                },
+                            label: {
+                                HStack {
+                                    Image(systemName: "arrow.forward.circle.fill")
+                                    Text(c.dropFirst())
+                                }
+                            }
+                        )
                     }
-                    .navigationBarTitle("\(problems)問目")
                 }
-                .navigationViewStyle(StackNavigationViewStyle())
+                .navigationBarTitle("\(problems)問目")
             }
+            .navigationViewStyle(StackNavigationViewStyle())
             .onDrop(of: ["public.json"], isTargeted: nil) {
                 providers, location in
                 if let item = providers.first {
-                    item.loadItem(forTypeIdentifier: "public.json", options: nil) { (urlData, error) in
+                    item.loadItem(forTypeIdentifier: "public.json", options: nil) { (urlData, _) in
                         if let url = urlData as? URL {
                             quizes = loadJson(url: url)
                             quiz = quizes.randomElement()!
                             corrects = 0
                             problems = 1
-                            // title = "新規問題セット"
-                        } else {
-                            // title = "couldn't convert url: \(String(describing: urlData))"
                         }
                     }
                     return true
